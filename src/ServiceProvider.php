428aa884ed7cae1dd1ff9e58f55bf44b8b8fa937
Andrey Helldar <helldar@ai-rus.com>
2019-04-26 11:39:52 +0300

    Added a Blade directives

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Helldar\Roles;
 
+use Illuminate\Support\Facades\Blade;
+
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     protected $defer = false;
@@ -9,5 +11,26 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+
+        $this->blade();
+    }
+
+    private function blade()
+    {
+        Blade::directive('role', function ($role) {
+            return "<?php if(\auth()->check() && \auth()->user()->hasRole($role)) { ?>";
+        });
+
+        Blade::directive('endrole', function () {
+            return "<?php } ?>";
+        });
+
+        Blade::directive('permission', function ($permission) {
+            return "<?php if(\auth()->check() && \auth()->user()->hasPermission($permission)) ?>";
+        });
+
+        Blade::directive('endpermission', function () {
+            return "<?php } ?>";
+        });
     }
 }
