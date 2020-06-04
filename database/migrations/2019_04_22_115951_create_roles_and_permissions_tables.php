d33785654ab2497093debea99fad842b8d46518d
Andrey Helldar <helldar@ai-rus.com>
2020-06-04 18:22:26 +0300

    Refactored migrations

--- database/migrations/2019_04_22_115951_create_roles_and_permissions_tables.php
+++ database/migrations/2019_04_22_115951_create_roles_and_permissions_tables.php
@@ -6,52 +6,49 @@ class CreateRolesAndPermissionsTables extends BaseMigration
 {
     public function up()
     {
-        $this->schema()
-            ->create($this->roles, function (Blueprint $table) {
-                $table->bigIncrements('id');
-                $table->string('name')->unique();
-                $table->timestamps();
-            });
+        $this->createTable($this->roles);
+        $this->createTable($this->permissions);
 
-        $this->schema()
-            ->create($this->permissions, function (Blueprint $table) {
-                $table->bigIncrements('id');
-                $table->string('name')->unique();
-                $table->timestamps();
-            });
-
-        $this->schema()
-            ->create($this->user_roles, function (Blueprint $table) {
-                $table->unsignedBigInteger('user_id');
-                $table->unsignedBigInteger('role_id');
-
-                $table->foreign('user_id')->references('id')->on($this->users)->onDelete('cascade');
-                $table->foreign('role_id')->references('id')->on($this->roles)->onDelete('cascade');
-
-                $table->primary(['user_id', 'role_id']);
-            });
-
-        $this->schema()
-            ->create($this->role_permissions, function (Blueprint $table) {
-                $table->unsignedBigInteger('role_id');
-                $table->unsignedBigInteger('permission_id');
-
-                $table->foreign('role_id')->references('id')->on($this->roles)->onDelete('cascade');
-                $table->foreign('permission_id')->references('id')->on($this->permissions)->onDelete('cascade');
-
-                $table->primary(['role_id', 'permission_id']);
-            });
+        $this->createPivot($this->user_roles, $this->users, $this->roles, 'user_id', 'role_id');
+        $this->createPivot($this->role_permissions, $this->roles, $this->permissions, 'role_id', 'permission_id');
     }
 
     public function down()
     {
         $this->schema()->disableForeignKeyConstraints();
 
-        $this->schema()->dropIfExists($this->role_permissions);
-        $this->schema()->dropIfExists($this->user_roles);
-        $this->schema()->dropIfExists($this->permissions);
-        $this->schema()->dropIfExists($this->roles);
+        $this->dropTables($this->role_permissions, $this->user_roles, $this->permissions, $this->roles);
 
         $this->schema()->enableForeignKeyConstraints();
+    }
+
+    protected function createTable(string $table)
+    {
+        $this->schema()
+            ->create($table, function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('name')->unique();
+                $table->timestamps();
+            });
+    }
+
+    protected function createPivot(string $table, string $first_table, string $second_table, string $first_key, string $second_key)
+    {
+        $this->schema()->create($table, function (Blueprint $table) use ($first_table, $second_table, $first_key, $second_key) {
+            $table->unsignedBigInteger($first_key);
+            $table->unsignedBigInteger($second_key);
+
+            $table->foreign($first_key)->references('id')->on($first_table)->onDelete('cascade');
+            $table->foreign($second_key)->references('id')->on($second_table)->onDelete('cascade');
+
+            $table->primary([$first_key, $second_key]);
+        });
+    }
+
+    protected function dropTables(...$tables)
+    {
+        foreach ($tables as $table) {
+            $this->schema()->dropIfExists($table);
+        }
     }
 }
