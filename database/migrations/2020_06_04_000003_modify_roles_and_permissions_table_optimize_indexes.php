fb19b89e93ea265131169222e7a51a130a7dad4f
Andrey Helldar <helldar@ai-rus.com>
2020-06-09 19:00:50 +0300

    Fixed migrations

--- database/migrations/2020_06_04_000003_modify_roles_and_permissions_table_optimize_indexes.php
+++ database/migrations/2020_06_04_000003_modify_roles_and_permissions_table_optimize_indexes.php
@@ -7,27 +7,27 @@ class ModifyRolesAndPermissionsTableOptimizeIndexes extends BaseMigration
 {
     public function up()
     {
-        $this->optimizeIndexes($this->user_role, 'user_id', 'role_id');
-        $this->optimizeIndexes($this->role_permission, 'role_id', 'permission_id');
+        $this->optimizeIndexes($this->user_role, 'user_id');
+        $this->optimizeIndexes($this->role_permission, 'role_id');
     }
 
     public function down()
     {
-        $this->revertIndexes($this->user_role, 'user_id', 'role_id');
-        $this->revertIndexes($this->role_permission, 'role_id', 'permission_id');
+        $this->revertIndexes($this->user_role, 'user_id');
+        $this->revertIndexes($this->role_permission, 'role_id');
     }
 
-    protected function optimizeIndexes(string $table, string $first_key, string $second_key)
+    protected function optimizeIndexes(string $table, string $first_key)
     {
-        $this->table($table, function (Blueprint $table) use ($first_key, $second_key) {
-            $table->index($first_key);
+        $this->table($table, function (Blueprint $table) use ($first_key) {
+            $table->index([$first_key]);
         });
     }
 
-    protected function revertIndexes(string $table, string $first_key, string $second_key)
+    protected function revertIndexes(string $table, string $first_key)
     {
-        $this->table($table, function (Blueprint $table) use ($first_key, $second_key) {
-            $table->primary([$first_key, $second_key]);
+        $this->table($table, function (Blueprint $table) use ($first_key) {
+            $table->dropIndex([$first_key]);
         });
     }
 }
